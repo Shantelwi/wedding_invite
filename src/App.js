62f99dd9093c5d92bb3399e8eae1ce111f37aa5f
Shantel Williams <williamsshantel35@yahoo.com>
2024-10-04 20:18:09 -0700

    tried changing background color

--- src/App.js
+++ src/App.js
@@ -1,51 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-// import Home from './components/Home';
-// import RSVPForm from './components/RSVPForm';
-// import Location from './components/Location';
-// import About from './components/AboutUs'
-// import FAQ from './components/FAQs';
-// import './App.css';
-// import './index.css';
-
-
-// function App() {
-//   return (
-//     <Router>
-//       <div className='relative'>
-//         <nav className=' text-cursive p-5 flex justify-evenly items-center '>
-//           <div>
-//             <Link to="/" className='nav-link text-2xl'>Home</Link>
-//           </div>
-//           <div>
-//             <Link to="/rsvp" className='nav-link text-2xl'>RSVP</Link>
-//           </div>
-//           <div>
-//             <Link to="/location" className='nav-link text-2xl'>Travel</Link>
-//           </div>
-//           <div>
-//             <Link to="/about" className='nav-link text-2xl'>Our Story</Link>
-//           </div>
-//           <div>
-//             <Link to="/faq" className='nav-link text-2xl'>FAQ's</Link>
-//           </div>
-//         </nav>
-//       </div>
-//       <hr></hr>
-//       <Routes>
-//         <Route path='/' element={<Home />} />
-//         <Route path='/rsvp' element={<RSVPForm />} />
-//         <Route path='/location' element={<Location />} />
-//         <Route path='/about' element={<About />} />
-//         <Route path='/faq' element={<FAQ />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
 // import React, { useState } from 'react';
 // import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 // import Home from './components/Home';
@@ -67,14 +19,16 @@
 //     <Router>
 //       <div className='relative'>
 //         <nav className='text-cursive p-5 flex justify-between items-center'>
-//           <div className='text-2xl md:hidden'>R+S</div> {/* Logo only on small screens */}
-//           <div className='hidden md:flex md:justify-evenly md:items-center'>
+//           <div className=' logo text-2xl md:hidden'>R+S</div> {/* Logo only on small screens */}
+//           <div className='hidden md:flex md:justify-center md:items-center w-full'>
 //             {/* Navigation links for desktop */}
-//             <Link to="/" className='nav-link text-2xl'>Home</Link>
-//             <Link to="/rsvp" className='nav-link text-2xl'>RSVP</Link>
-//             <Link to="/location" className='nav-link text-2xl'>Travel</Link>
-//             <Link to="/about" className='nav-link text-2xl'>Our Story</Link>
-//             <Link to="/faq" className='nav-link text-2xl'>FAQ's</Link>
+//             <div className='flex space-x-8'>
+//               <Link to="/" className='nav-link text-4xl'>Home </Link> 
+//               <Link to="/rsvp" className='nav-link text-4xl'> RSVP </Link> 
+//               <Link to="/location" className='nav-link text-4xl'> Travel </Link> 
+//               <Link to="/about" className='nav-link text-4xl'> Our Story </Link> 
+//               <Link to="/faq" className='nav-link text-4xl'> FAQ's</Link>
+//             </div>
 //           </div>
 //           <div className='md:hidden' onClick={toggleMenu}>
 //             {/* Hamburger Icon */}
@@ -127,19 +81,23 @@ function App() {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <Router>
       <div className='relative'>
-        <nav className='text-cursive p-5 flex justify-between items-center'>
-          <div className=' logo text-2xl md:hidden'>R+S</div> {/* Logo only on small screens */}
+        <nav className='text-cursive p-5 flex justify-between items-center bg-white'>
+          <div className='logo text-2xl md:hidden'>R+S</div> {/* Logo only on small screens */}
           <div className='hidden md:flex md:justify-center md:items-center w-full'>
             {/* Navigation links for desktop */}
             <div className='flex space-x-8'>
-              <Link to="/" className='nav-link text-4xl'>Home </Link> 
-              <Link to="/rsvp" className='nav-link text-4xl'> RSVP </Link> 
-              <Link to="/location" className='nav-link text-4xl'> Travel </Link> 
-              <Link to="/about" className='nav-link text-4xl'> Our Story </Link> 
-              <Link to="/faq" className='nav-link text-4xl'> FAQ's</Link>
+              <Link to="/" className='nav-link text-4xl'>Home</Link>
+              <Link to="/rsvp" className='nav-link text-4xl'>RSVP</Link>
+              <Link to="/location" className='nav-link text-4xl'>Travel</Link>
+              <Link to="/about" className='nav-link text-4xl'>Our Story</Link>
+              <Link to="/faq" className='nav-link text-4xl'>FAQ's</Link>
             </div>
           </div>
           <div className='md:hidden' onClick={toggleMenu}>
@@ -153,12 +111,12 @@ function App() {
         </nav>
         {/* Mobile Menu */}
         {isOpen && (
-          <div className='flex-col md:hidden'>
-            <Link to="/" className='nav-link text-2xl'>Home</Link>
-            <Link to="/rsvp" className='nav-link text-2xl'>RSVP</Link>
-            <Link to="/location" className='nav-link text-2xl'>Travel</Link>
-            <Link to="/about" className='nav-link text-2xl'>Our Story</Link>
-            <Link to="/faq" className='nav-link text-2xl'>FAQ's</Link>
+          <div className='flex-col md:hidden bg-white p-5'>
+            <Link to="/" className='nav-link text-2xl' onClick={closeMenu}>Home</Link>
+            <Link to="/rsvp" className='nav-link text-2xl' onClick={closeMenu}>RSVP</Link>
+            <Link to="/location" className='nav-link text-2xl' onClick={closeMenu}>Travel</Link>
+            <Link to="/about" className='nav-link text-2xl' onClick={closeMenu}>Our Story</Link>
+            <Link to="/faq" className='nav-link text-2xl' onClick={closeMenu}>FAQ's</Link>
           </div>
         )}
       </div>
@@ -175,8 +133,6 @@ function App() {
 }
 
 export default App;
-
-
 
 
 
