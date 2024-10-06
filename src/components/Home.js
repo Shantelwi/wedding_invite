c22db7f9e2e9c6529d0f4209d68523789236d478
Shantel Williams <williamsshantel35@yahoo.com>
2024-10-05 21:06:45 -0700

    removed footer from home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import Slider from 'react-slick';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import Footer from './Footer';
 
 const Home = () => {
     const carouselSettings = {
